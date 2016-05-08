3b1e7a0a98d9829f3424cbc0303b883bbf583e68
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-05-08 13:42:05 +0200

    addToRead and drop elements implementated

--- edi-a005-2016/src/ule/edi/recursive/SingleLinkedListImpl.java
+++ edi-a005-2016/src/ule/edi/recursive/SingleLinkedListImpl.java
@@ -1,71 +1,97 @@
 package ule.edi.recursive;
 
 public class SingleLinkedListImpl<T> extends AbstractSingleLinkedList<T> {
-	
-	public SingleLinkedListImpl() {
 
-	}
-	//
+	public SingleLinkedListImpl() {}
 	
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "unchecked", "unused" })
 	public SingleLinkedListImpl(T... values) {
 		int a;
 		for (T i : values) {
-
 			addToRear(i);
 		}
 	}
 
 	@Override
 	public void addToRear(T content) {
-
-		// TODO Implementar de forma recursiva
+		Node<T> insert = new Node<T>(content);
+		if(first==null){
+			first = new Node<T>(content);
+		}else{
+			addToRearRecursive(insert,this.first);
+		}
+	}
+	private void addToRearRecursive(Node<T> insert, Node<T> current){
+		if(current.next==null){
+			current.next = insert;
+		}else{
+			addToRearRecursive(insert,current.next);
+		}
 	}
 
 	@Override
 	public boolean isAscend() {
-		
-		// TODO Implementar de forma recursiva
+
 		return false;
 	}
+	
+	private boolean isAscendRecursive(Node<T> current){
+		return false;
+	}
+	
 
 	@Override
 	public boolean isDescend() {
-		
+
 		// TODO Implementar de forma recursiva
 		return false;
 	}
 
 	@Override
 	public boolean isEqualStructure(AbstractSingleLinkedList<T> other) {
-		
+
 		// TODO Implementar de forma recursiva
 		return false;
 	}
 
 	@Override
 	public void dropElements(int n) {
-		
-		// TODO Implementar de forma recursiva
+	
+		if(n>0 && this.first!= null){
+			dropElementsRecursive(n, first, 0);
+		}
+	}
+	private void dropElementsRecursive(int n, Node<T> current, int cout){
+		if(cout==n){
+			this.first = current;
+			return;
+		}else if(current.next==null){
+			//Se ha acabado la lista, devolvemos la vacia
+			return;
+		}else{
+			cout++;
+			current = current.next;
+			dropElementsRecursive(n, current, cout);
+		}
 	}
 
 	@Override
 	public SimpleListADT<T> reverse() {
-		
+
 		// TODO Implementar de forma recursiva
 		return null;
 	}
 
 	@Override
 	public SimpleListADT<T> repeatAllElements() {
-		
+
 		// TODO Implementar de forma recursiva
 		return null;
 	}
 
 	@Override
 	public SimpleListADT<T> repeatNElements(int n) {
-		
+
 		// TODO Implementar de forma recursiva
 		return null;
 	}
--- edi-a005-2016/src/ule/edi/recursive/SingleLinkedListImplTests.java
+++ edi-a005-2016/src/ule/edi/recursive/SingleLinkedListImplTests.java
@@ -1,7 +1,10 @@
 package ule.edi.recursive;
 
+import static org.junit.Assert.*;
+
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SingleLinkedListImplTests {
@@ -10,6 +13,8 @@ public class SingleLinkedListImplTests {
 	private SimpleListADT<String> lS;
 
 	private SimpleListADT<String> lSABC;
+	
+	private static final String[] ABC_ = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 	
 	@Before
 	public void setupLists() {
@@ -44,6 +49,16 @@ public class SingleLinkedListImplTests {
 		
 		lS.addFirst("C");
 		Assert.assertEquals("[C, B, A]", lS.toString());
+	}
+	
+	@Test
+	public void test00(){
+		for (int i = 0; i < ABC_.length; i++) {
+			lS.addToRear(ABC_[i]);
+		}
+		assertEquals("[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]",lS.toString());
+		lS.dropElements(30);
+		System.out.println(lS);
 	}
 	
 	
