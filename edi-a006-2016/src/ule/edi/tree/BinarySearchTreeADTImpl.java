7ce9e3e294c2e0c8c658e451d271d65cc526424f
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-05-18 17:54:50 +0200

    Implemented firts method

--- edi-a006-2016/src/ule/edi/tree/BinarySearchTreeADTImpl.java
+++ edi-a006-2016/src/ule/edi/tree/BinarySearchTreeADTImpl.java
@@ -9,18 +9,18 @@ import java.util.Queue;
 import ule.edi.EmptyCollectionException;
 
 /**
- * Árbol binario de búsqueda (binary search tree, BST).
+ * Ã�rbol binario de bÃºsqueda (binary search tree, BST).
  * 
  * @author profesor
  * 
  * @param <T>
- *            tipo de la información en cada nodo, comparable.
+ *            tipo de la informaciÃ³n en cada nodo, comparable.
  */
 public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
-		AbstractBinaryTreeADT<T> {
+AbstractBinaryTreeADT<T> {
 
 	/**
-	 * Devuelve el árbol binario de búsqueda izquierdo.
+	 * Devuelve el Ã¡rbol binario de bÃºsqueda izquierdo.
 	 * 
 	 * @return
 	 */
@@ -31,9 +31,9 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	private void setLeftBST(BinarySearchTreeADTImpl<T> left) {
 		this.leftSubtree = left;
 	}
-	
+
 	/**
-	 * Devuelve el árbol binario de búsqueda derecho.
+	 * Devuelve el Ã¡rbol binario de bÃºsqueda derecho.
 	 * 
 	 * @return
 	 */
@@ -44,14 +44,14 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	private void setRightBST(BinarySearchTreeADTImpl<T> right) {
 		this.rightSubtree = right;
 	}
-	
+
 	/**
-	 * Árbol BST vacío
+	 * Ã�rbol BST vacÃ­o
 	 */
 	public BinarySearchTreeADTImpl() {
-		
+
 		setContent(null);
-		
+
 		setLeftBST(null);
 		setRightBST(null);
 	}
@@ -59,9 +59,9 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	private BinarySearchTreeADTImpl<T> emptyBST() {
 		return new BinarySearchTreeADTImpl<T>();
 	}
-	
+
 	/**
-	 * Inserta todos los elementos de una colección en el árbol.
+	 * Inserta todos los elementos de una colecciÃ³n en el Ã¡rbol.
 	 * 
 	 * @param elements
 	 *            valores a insertar.
@@ -74,76 +74,76 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 				throw new IllegalArgumentException("No se aceptan elementos nulos");
 			}
 		}
-		
+
 		for (T j : elements) {
 			insert(j);
 		}
 	}
 
 	/**
-	 * Inserta todos los elementos de un array en el árbol.
+	 * Inserta todos los elementos de un array en el Ã¡rbol.
 	 * 
 	 * @param elements elementos a insertar.
 	 */
 	public void insert(T ... elements) {
-		
+
 		//	O todos o ninguno; si alguno es 'null', ni siquiera se comienza a insertar
 		for (T i : elements) {
 			if (i == null) {
 				throw new IllegalArgumentException("No se aceptan elementos nulos");
 			}
 		}
-		
+
 		for (T j : elements) {
 			insert(j);
 		}
 	}
-	
+
 	/**
-	 * Inserta (como hoja) un nuevo elemento en el árbol de búsqueda. Si el
-	 * elemento ya existe en el árbol NO lo inserta.
+	 * Inserta (como hoja) un nuevo elemento en el Ã¡rbol de bÃºsqueda. Si el
+	 * elemento ya existe en el Ã¡rbol NO lo inserta.
 	 * 
 	 * @param element
 	 *            valor a insertar.
 	 */
 	public void insert(T element) {
-		
+
 		if (element == null) {
 			throw new IllegalArgumentException("No se aceptan elementos nulos");
 		}
-		
+
 		if (isEmpty()) {
-		
-			// Ya no es vacío, ahora es hoja
+
+			// Ya no es vacÃ­o, ahora es hoja
 			setContent(element);
-			
-			//	Sus hijos son vacíos
+
+			//	Sus hijos son vacÃ­os
 			setLeftBST(emptyBST());
 			setRightBST(emptyBST());
 		} else {
-			
-			// Compara con el valor de la raíz
+
+			// Compara con el valor de la raÃ­z
 			int diff = element.compareTo(content);
 
 			if (diff == 0) {
-				// Ya está en el árbol
+				// Ya estÃ¡ en el Ã¡rbol
 				return;
 			}
 
 			if (diff < 0) {
-				// Es menor, irá en el sub-árbol izquierdo
+				// Es menor, irÃ¡ en el sub-Ã¡rbol izquierdo
 				getLeftBST().insert(element);
 			}
-			
+
 			if (diff > 0) {
-				// Es mayor, irá al sub-árbol derecho
+				// Es mayor, irÃ¡ al sub-Ã¡rbol derecho
 				getRightBST().insert(element);
 			}
 		}
 	}
 
 	/**
-	 * Elimina los elementos de la colección del árbol.
+	 * Elimina los elementos de la colecciÃ³n del Ã¡rbol.
 	 * 
 	 * @param elements
 	 *            elementos a eliminar.
@@ -155,7 +155,7 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	}
 
 	/**
-	 * Elimina los valores en un array del árbol.
+	 * Elimina los valores en un array del Ã¡rbol.
 	 * 
 	 * @param elements valores a eliminar.
 	 */
@@ -164,11 +164,11 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 			withdraw(e);
 		}
 	}
-	
+
 	/**
-	 * Devuelve el máximo en el árbol.
+	 * Devuelve el mÃ¡ximo en el Ã¡rbol.
 	 * 
-	 * @return máximo valor en el árbol.
+	 * @return mÃ¡ximo valor en el Ã¡rbol.
 	 * @throws EmptyCollectionException 
 	 */
 	public T findMax() throws EmptyCollectionException {
@@ -180,16 +180,16 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 				return getRightBST().findMax();
 			}
 		} else {
-			throw new EmptyCollectionException("No se puede encontrar el máximo de un árbol vacío");
+			throw new EmptyCollectionException("No se puede encontrar el mÃ¡ximo de un Ã¡rbol vacÃ­o");
 		}
 	}
-	
+
 	/**
-	 * Elimina un elemento del árbol.
+	 * Elimina un elemento del Ã¡rbol.
 	 * 
 	 * @param element
 	 *            elemento a eliminar.
-	 * @throws NoSuchElementException si el elemento a eliminar no está en el árbol           
+	 * @throws NoSuchElementException si el elemento a eliminar no estÃ¡ en el Ã¡rbol           
 	 */
 	public void withdraw(T element) {
 
@@ -199,7 +199,7 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 		}
 
 		if (!isEmpty()) {
-			// Compara con la información en la raíz
+			// Compara con la informaciÃ³n en la raÃ­z
 			int diff = element.compareTo(getContent());
 
 			if (diff == 0) {
@@ -212,35 +212,35 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 					setLeftBST(null);
 					setRightBST(null);
 				}
-				// Si tiene sub-árbol izquierdo pero no derecho
+				// Si tiene sub-Ã¡rbol izquierdo pero no derecho
 				else if (!getLeftBST().isEmpty() && getRightBST().isEmpty()) {
 					setContent(getLeftBST().getContent());
 
 					setRightBST(getLeftBST().getRightBST());
 					setLeftBST(getLeftBST().getLeftBST());
 				}
-				// Si tiene sub-árbol derecho pero no izquierdo
+				// Si tiene sub-Ã¡rbol derecho pero no izquierdo
 				else if (getLeftBST().isEmpty() && !getRightBST().isEmpty()) {
 					setContent(getRightBST().getContent());
 					setLeftBST(getRightBST().getLeftBST());
 					setRightBST(getRightBST().getRightBST());
 
 				}
-				// // Si tiene sub-árbol izquierdo y derecho
+				// // Si tiene sub-Ã¡rbol izquierdo y derecho
 				else if (!getLeftBST().isEmpty() && !getRightBST().isEmpty()) {
 
 					try {
 						T searchMax = getLeftBST().findMax(); // busca el mayor
-																// de los
-																// menores
+						// de los
+						// menores
 						// sustituye el elemento que quiere eliminar por el
 						// mayor de los menores
 						content = searchMax;
-						// elimina el mayor de los menores que no tendrá hijo
+						// elimina el mayor de los menores que no tendrÃ¡ hijo
 						// derecho y por tanto termina sin recursividad
 						getLeftBST().withdraw(searchMax);
 					} catch (EmptyCollectionException e) {
-						// No debería ser posible
+						// No deberÃ­a ser posible
 						throw new IllegalStateException(e);
 					}
 				}
@@ -255,129 +255,137 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 					getLeftBST().withdraw(element);
 			}
 		}
-		// si el elemento no está en el árbol
+		// si el elemento no estÃ¡ en el Ã¡rbol
 		else
 			throw new NoSuchElementException();
 	}					
-	
+
 	/**
-	 * Devuelve el número de árboles vacíos en este árbol.
+	 * Devuelve el nÃºmero de Ã¡rboles vacÃ­os en este Ã¡rbol.
 	 * 
-	 * Por ejemplo, sea un árbol "A" {10, {5, ∅, ∅}, {20, ∅, {30, ∅, ∅}}}:
+	 * Por ejemplo, sea un Ã¡rbol "A" {10, {5, âˆ…, âˆ…}, {20, âˆ…, {30, âˆ…, âˆ…}}}:
 	 * 
 	 * 10
 	 * |  5
-	 * |  |  ∅
-	 * |  |  ∅
+	 * |  |  âˆ…
+	 * |  |  âˆ…
 	 * |  20
-	 * |  |  ∅
+	 * |  |  âˆ…
 	 * |  |  30
-	 * |  |  |  ∅
-	 * |  |  |  ∅
+	 * |  |  |  âˆ…
+	 * |  |  |  âˆ…
 	 * 
-	 * el resultado sería 5.
+	 * el resultado serÃ­a 5.
 	 * 
-	 * @return número de árboles vacíos.
+	 * @return nÃºmero de Ã¡rboles vacÃ­os.
 	 */
 	public long countEmpty() {
+		return countEmptyRec(this.leftSubtree)+countEmptyRec(this.leftSubtree);
+	}
+	
+	public long countEmptyRec(AbstractBinaryTreeADT<T> subtree){
+		if(subtree.isEmpty()){
+			return 1;
+		}else{
+			return countEmptyRec(subtree.rightSubtree)+countEmptyRec(subtree.leftSubtree);
+		}
 		
-		// TODO Implementar el método
-		return 0;
 	}
 	
 	/**
-	 * Indica el número de elementos en niveles impares.
+	 * Indica el nÃºmero de elementos en niveles impares.
 	 * 
-	 * La raíz de éste árbol está en nivel 1.
+	 * La raÃ­z de Ã©ste Ã¡rbol estÃ¡ en nivel 1.
 	 * 
-	 * Por ejemplo, sea un árbol "A" {10, {5, ∅, ∅}, {20, ∅, {30, ∅, ∅}}}:
+	 * Por ejemplo, sea un Ã¡rbol "A" {10, {5, âˆ…, âˆ…}, {20, âˆ…, {30, âˆ…, âˆ…}}}:
 	 * 
 	 * 10
 	 * |  5
-	 * |  |  ∅
-	 * |  |  ∅
+	 * |  |  âˆ…
+	 * |  |  âˆ…
 	 * |  20
-	 * |  |  ∅
+	 * |  |  âˆ…
 	 * |  |  30
-	 * |  |  |  ∅
-	 * |  |  |  ∅
+	 * |  |  |  âˆ…
+	 * |  |  |  âˆ…
 	 * 
-	 * el resultado sería 2.
+	 * el resultado serÃ­a 2.
 	 * 
-	 * @return número de elementos en niveles impares.
+	 * @return nÃºmero de elementos en niveles impares.
 	 */
+	
 	public long countOddLevelElements() {
-		
-		// TODO Implementar el método
+
 		return 0;
 	}
-	
+
 	/**
-	 * Acumula en pre-orden, una lista con los pares 'padre-hijo' en este árbol.
+	 * Acumula en pre-orden, una lista con los pares 'padre-hijo' en este Ã¡rbol.
 	 * 
-	 * Por ejemplo, sea un árbol "A":
+	 * Por ejemplo, sea un Ã¡rbol "A":
 	 * 
-	 * {10, {5, {2, ∅, ∅}, ∅}, {20, ∅, {30, ∅, ∅}}}
+	 * {10, {5, {2, âˆ…, âˆ…}, âˆ…}, {20, âˆ…, {30, âˆ…, âˆ…}}}
 	 * 
-     * 10
-     * |  5
-     * |  |  2
-     * |  |  |  ∅
-     * |  |  |  ∅
-     * |  |  ∅
-     * |  20
-     * |  |  ∅
-     * |  |  30
-     * |  |  |  ∅
-     * |  |  |  ∅
-     * 
-	 * el resultado sería una lista de cadenas:
+	 * 10
+	 * |  5
+	 * |  |  2
+	 * |  |  |  âˆ…
+	 * |  |  |  âˆ…
+	 * |  |  âˆ…
+	 * |  20
+	 * |  |  âˆ…
+	 * |  |  30
+	 * |  |  |  âˆ…
+	 * |  |  |  âˆ…
+	 * 
+	 * el resultado serÃ­a una lista de cadenas:
 	 * 
 	 * 	[(10,5), (5,2), (10,20), (20,30)]
 	 * 
 	 * @param buffer lista con el resultado.
 	 */
-	public void parentChildPairs(List<String> buffer) {
-		
-		// TODO Implementar el método
-	}
 	
+	public void parentChildPairs(List<String> buffer) {
+
+		// TODO Implementar el mÃ©todo
+	}
+
 	/**
-	 * Devuelve el contenido del nodo alcanzado desde la raíz
-	 * de éste árbol, con el camino dado.
+	 * Devuelve el contenido del nodo alcanzado desde la raÃ­z
+	 * de Ã©ste Ã¡rbol, con el camino dado.
 	 * 
 	 * Si se codifica "bajar por la izquierda" como "0" y
 	 * "bajar por la derecha" como "1", el camino desde un 
-	 * nodo N hasta un nodo M (en uno de sus sub-árboles) será la
-	 * cadena de 0s y 1s que indica cómo llegar desde N hasta M.
-     *
-     * Se define también el camino vacío desde un nodo N hasta
-     * él mismo, como cadena vacía.
+	 * nodo N hasta un nodo M (en uno de sus sub-Ã¡rboles) serÃ¡ la
+	 * cadena de 0s y 1s que indica cÃ³mo llegar desde N hasta M.
+	 *
+	 * Se define tambiÃ©n el camino vacÃ­o desde un nodo N hasta
+	 * Ã©l mismo, como cadena vacÃ­a.
 
-	 * Por ejemplo, sea un árbol "A" {10, {5, ∅, ∅}, {20, ∅, {30, ∅, ∅}}}:
+	 * Por ejemplo, sea un Ã¡rbol "A" {10, {5, âˆ…, âˆ…}, {20, âˆ…, {30, âˆ…, âˆ…}}}:
 	 * 
 	 * 10
 	 * |  5
-	 * |  |  ∅
-	 * |  |  ∅
+	 * |  |  âˆ…
+	 * |  |  âˆ…
 	 * |  20
-	 * |  |  ∅
+	 * |  |  âˆ…
 	 * |  |  30
-	 * |  |  |  ∅
-	 * |  |  |  ∅
+	 * |  |  |  âˆ…
+	 * |  |  |  âˆ…
 	 * 
 	 * Entonces se tiene que A.getContentWithPath("11") es 30 y
 	 * que A.getContentWithPath("") es 10.
 	 * 
-	 * @param path camino a seguir desde la raíz.
+	 * @param path camino a seguir desde la raÃ­z.
 	 * @return contenido del nodo alcanzado.
-	 * @throws NoSuchElementException si el camino no alcanza un nodo en el árbol
-	 * @throws IllegalArgumentException si el camino no contiene sólamente 0s y 1s
+	 * @throws NoSuchElementException si el camino no alcanza un nodo en el Ã¡rbol
+	 * @throws IllegalArgumentException si el camino no contiene sÃ³lamente 0s y 1s
 	 */
 	public T getContentWithPath(String path) {
-		
-		// TODO Implementar el método
+
+		// TODO Implementar el mÃ©todo
 		return null;
 	}	
-	
+
 }
